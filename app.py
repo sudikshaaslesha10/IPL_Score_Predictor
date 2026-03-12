57389a8a030dd8ea35af95ff1b5a8bce26391399
Sudiksha Aslesha <sudiksha.aslesha10@gmail.com>
2026-03-12 15:48:55 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,22 @@ model = joblib.load("model.pkl")
 
 st.title("IPL Score Predictor")
 
+# Team list
+teams = [
+"Chennai Super Kings",
+"Delhi Capitals",
+"Kings XI Punjab",
+"Kolkata Knight Riders",
+"Mumbai Indians",
+"Rajasthan Royals",
+"Royal Challengers Bangalore",
+"Sunrisers Hyderabad"
+]
+
+# Team selection
+batting_team = st.selectbox("Batting Team", teams)
+bowling_team = st.selectbox("Bowling Team", teams)
+
 overs = st.number_input("Overs Completed",0.0,20.0)
 runs = st.number_input("Current Runs",0)
 wickets = st.number_input("Wickets Lost",0,10)
